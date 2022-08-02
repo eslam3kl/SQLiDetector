2795cba90f6cf145a605c66c9f9fb40b2e9e2e4f
knassar702 <knassar702@gmail.com>
2022-08-02 19:11:47 +0200

    print logo

---
+++ sqlidetector.py
@@ -0,0 +1,24 @@
+#!/usr/bin/pythno3
+from core.cli import cli_opts, print_logo
+from core.app import Scanner
+
+
+def main():
+    print_logo()
+    opts = cli_opts()
+    targets = opts.file
+    timeout = opts.timeout
+    workers = opts.workers
+
+    if opts.proxy:
+        proxy = {"http": opts.proxy, "https": opts.proxy}
+    else:
+        proxy = {}
+    app = Scanner(timeout, proxy)
+    app.start(targets, workers)
+    if opts.output:
+        app.write_report(opts.output)
+
+
+if __name__ == "__main__":
+    main()
