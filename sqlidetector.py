b8e995ee523fc99d38f1255fbb76d9c5bd817e74
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2022-08-03 19:06:06 +0300

    Added --stdin option

--- sqlidetector.py
+++ sqlidetector.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
+from sys import stdin, argv
 from core.cli import cli_opts, print_logo
 from core.app import Scanner
+from rich.console import Console
 
+console = Console(style="bold red")
 
 def main():
     print_logo()
     opts = cli_opts()
-    targets = opts.file
+    if opts.file:
+        targets = opts.file
+    elif opts.stdin:
+        targets = [url for url in stdin.readlines() if "?" in url]
+    else:
+        console.print(f"\n[!] Usage: {argv[0]} -h\n")
+        exit(1)
     timeout = opts.timeout
     workers = opts.workers
 
